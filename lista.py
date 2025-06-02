ae8bee368f69b024291fece3ff0dae97da9bb5ca
yuri <almeidayr88@gmail.com>
2025-06-02 15:52:10 -0300

    Add files via upload

---
+++ lista.py
@@ -0,0 +1,66 @@
+# Exercício 1
+
+# numeros =[]
+
+# for i in range(1,20):
+#     numeros.append(i)
+
+# print(numeros)
+
+# Exercício 2
+
+# lista = [10, 20, 30]
+
+# print('inicio da lista',lista)
+# lista.insert(0,1)
+# print('inserindo com insert',lista)
+
+
+# lista.append(7)
+# print('inserindo com append',lista)
+
+# Exercício 3
+
+# lista = ['a', 'b' ,'c']
+
+
+# lista[1] = 'x' 
+
+# print(lista)
+
+# Exercício 4
+
+# lista = [5, 10, 15, 20]
+# del lista[2] 
+# print(lista)
+
+# Exercício 5 
+
+# lista = ['maçã', 'banana', 'laranja']
+# lista.remove("banana")
+# print(lista)
+
+# Exercício 6
+
+# lista = [100, 200, 300, 400]
+# teste=  lista.pop(3)
+# print(teste)
+
+# Exercício 7
+
+# lista = ['python', 'java', 'c++']
+
+# teste= lista.pop(1)
+# print(teste)
+
+# Exercício 8
+
+# lista = [1, 2, 3, 4, 5]
+
+# lista.clear()
+# print(lista)
+
+# Exercício 9
+
+lista = ['a', 'b', 'd']
+lista.insert
